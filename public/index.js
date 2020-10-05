307064ec37313ece5362f83b328c1ac26d917b86
Andresito de Guzman <andresitomyemail@gmail.com>
2020-10-06 02:34:01 +0800

    feat: add task methods

--- public/index.js
+++ public/index.js
@@ -4,5 +4,75 @@
  * 
  * PWA Pilipinas
  */
+$(document).ready(() => {
+    console.log('Mabuhay PWA Pilipinas!');
+    $('.preloader').fadeOut();
+});
 
-console.log('Mabuhay PWA Pilipinas!');
+/**
+ * getTasks
+ */
+const getTasks = () => {
+    try {
+        return (JSON.parse(localStorage.getItem('todo'))).sort((a,b) => a.id - b.id) || [];
+    } catch (e) {
+        return [];
+    }
+};
+
+/**
+ * getTasksList
+ */
+const getTasksList = () => {
+    try {
+        return (JSON.parse(localStorage.getItem('todo'))).sort((a,b) => b.id - a.id) || [];
+    } catch (e) {
+        return [];
+    }
+};
+
+/**
+ * addTask
+ * @param {string} task 
+ */
+const addTask = (task) => {
+    const taskList = getTasks() || [];
+    taskList.push({ id: Date.now(), title: task, isDone: false });
+    localStorage.setItem('todo', JSON.stringify(taskList));
+    return taskList;
+};
+
+/**
+ * deleteTask
+ * @param {string | number} taskId 
+ */
+const deleteTask = (taskId) => {
+    let taskList = getTasks() || [];
+    taskList = taskList.filter(e => { if(e.id !== +taskId) return e });
+    localStorage.setItem('todo', JSON.stringify(taskList));
+    return taskList;
+};
+
+/**
+ * getTask
+ * @param {string | number} taskId 
+ */
+const getTask = (taskId) => {
+    let taskList = getTasks() || [];
+    task = taskList.find(e => e.id === +taskId);
+    return task || {};
+};
+
+/**
+ * toggleTask
+ * @param {string | number} taskId 
+ */
+const toggleTask = (taskId) => {
+    let taskList = getTasks() || [];
+    taskList = taskList.map(e => {
+        if(e.id === +taskId) e.isDone = e.isDone ? false : true;
+        return e;
+    })
+    localStorage.setItem('todo', JSON.stringify(taskList));
+    return taskList;
+};
