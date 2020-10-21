233796357f3bd07fd1b869a0aa54b52310de21e5
Andresito de Guzman <andresitomyemail@gmail.com>
2020-10-22 01:49:11 +0800

    feat: update index.js

--- public/index.js
+++ public/index.js
@@ -9,14 +9,6 @@ $(document).ready(() => {
     $('.preloader').fadeOut();
     listUncompletedTasks();
     listCompletedTasks();
-
-     /* Delete task event listener */
-     $('.delete').click(function() {
-        const id = $(this).attr('data-id');
-        deleteTask(id);
-        $('#' + id).remove(); // remove task from UI
-        M.toast({ html: 'Task successfully deleted!', classes: 'green darken-1' });
-     });
 });
 
 //add Todo from input
@@ -68,6 +60,15 @@ const getTasksList = () => {
 };
 
 /**
+ * refreshView
+ */
+const refreshView = () => {
+    listUncompletedTasks();
+    listCompletedTasks();
+    return;
+};
+
+/**
  * addTask
  * @param {string} task 
  */
@@ -75,6 +76,7 @@ const addTask = (task) => {
     const taskList = getTasks() || [];
     taskList.push({ id: Date.now(), title: task, isDone: false });
     localStorage.setItem('todo', JSON.stringify(taskList));
+    refreshView();
     return taskList;
 };
 
@@ -110,6 +112,7 @@ const toggleTask = (taskId) => {
         return e;
     })
     localStorage.setItem('todo', JSON.stringify(taskList));
+    refreshView();
     return taskList;
 };
 
@@ -119,10 +122,10 @@ const toggleTask = (taskId) => {
 const listUncompletedTasks = () => {
     let tasksTodo = getTasksList() || [];
     tasksTodo = tasksTodo.filter(e => !e.isDone);
+    $('.tasks-todo-list').html('');
     tasksTodo.forEach(function(task) {
         displayTaskItemUI(task, '.tasks-todo');
     });
-    $('.tasks-todo .task-item').last().remove();
 };
 
 /**
@@ -131,22 +134,74 @@ const listUncompletedTasks = () => {
 const listCompletedTasks = () => {
     let tasksDone = getTasksList() || [];
     tasksDone = tasksDone.filter(e => e.isDone);
+    $('.tasks-done-list').html('');
     tasksDone.forEach(function(task) {
         displayTaskItemUI(task, '.tasks-done');
     });
-    $('.tasks-done .task-item').last().remove();
+};
+
+/**
+ * setAsDone
+ * @param {number} id 
+ */
+const setAsDone = (id) => {
+    toggleTask(id);
+    refreshView();
+    M.toast({ html: 'Task done!', classes: 'green darken-1' });
+};
+
+/**
+ * setAsTodo
+ * @param {number} id 
+ */
+const setAsTodo = (id) => {
+    toggleTask(id);
+    refreshView();
+    M.toast({ html: 'Task set as to-do!', classes: 'green darken-1' });
+};
+
+/**
+ * deleteTaskUI
+ * @param {number} id 
+ */
+const deleteTaskUI = (id) => {
+    deleteTask(id);
+    refreshView();
+    M.toast({ html: 'Task successfully deleted!', classes: 'green darken-1' });
 };
 
 /**
  * displayTaskItemUI
- * @param {string} task 
+ * @param {string} task
  * @param {string} taskListContainer
  */
 const displayTaskItemUI = (task, taskListContainer) => {
-    const template = $(taskListContainer + ' .task-item').last();
-    let taskItemUI = template.clone();
-    taskItemUI.attr('id', task.id);
-    taskItemUI.find('.action').attr('data-id', task.id);
-    taskItemUI.find('.task-title').text(task.title);
-    $(taskListContainer + ' .title').after(taskItemUI);
+    let tpl = '';
+    if(taskListContainer === '.tasks-todo') {
+        tpl = `
+        <div class="task-item card-content blue-grey lighten-4 z-depth-0" id="${task.id}">
+            <div class="row valign-wrapper">
+                <div class="col m9 s8 task-title">${task.title}</div>
+                <div class="col m3 s4 options right-align">
+                    <a data-id="${task.id}" class="btn-small action check-todo blue darken-1 white-text" onclick="setAsDone(${task.id})"><i class="material-icons">check</i></a>
+                    <a data-id="${task.id}" class="btn-small action delete red darken-1 white-text" onclick="deleteTaskUI(${task.id})"><i class="material-icons">delete</i></a>
+                </div>
+            </div>
+        </div>
+        `;
+        $('.tasks-todo-list').append(tpl);
+    } else {
+        tpl = `
+        <div class="task-item card-content blue-grey lighten-4 z-depth-0" id="${task.id}">
+            <div class="row valign-wrapper">
+                <div class="col m9 s8 task-title">${task.title}</div>
+                    <div class="col m3 s4 options right-align">
+                        <a data-id="${task.id}" class="btn-small action check-done blue darken-1 white-text" onclick="setAsTodo(${task.id})"><i class="material-icons">check</i></a>
+                        <a data-id="${task.id}" class="btn-small action delete red darken-1 white-text" onclick="deleteTaskUI(${task.id})"><i class="material-icons">delete</i></a>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+        $('.tasks-done-list').append(tpl);
+    }
 };
